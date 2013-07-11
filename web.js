a870a0044b4ad7d87813b239e82387cd3fecdff5
Ubuntu <ubuntu@ip-10-164-55-159.ec2.internal>
2013-07-11 01:21:57 +0000

    Test3

--- web.js
+++ web.js
@@ -4,14 +4,14 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send('Hello World3!');
-  fs.readFileSync('index.html', 'utf8', function (err, data) {
-    if (err) throw err;
-    console.log(data);
-    response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-    response.write(data);
-    response.end();
-  });  
+  response.send('Hello World3!');
+  //fs.readFileSync('index.html', 'utf8', function (err, data) {
+  //  if (err) throw err;
+  //  console.log(data);
+  //  response.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+  //  response.write(data);
+  //  response.end();
+  //});  
 });
 
 var port = process.env.PORT || 5000;
