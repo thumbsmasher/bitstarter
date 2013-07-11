569f1dcc5bc4e3dccf16ebf26ea272b2865b1fe2
Ubuntu <ubuntu@ip-10-164-55-159.ec2.internal>
2013-07-11 00:29:02 +0000

    Commit for deploy to heroku

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  //response.send('Hello World2!');
+  fs.readFileSync('index.html', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+  });  
 });
 
 var port = process.env.PORT || 5000;
